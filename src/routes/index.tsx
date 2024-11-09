ca41a5f45c09e1b1d53a53903c857d37237e286f
hellosonic-r <hellommmh@gmail.com>
2024-11-09 19:35:02 +0900

    remove: MyPage -> ProfilePage로 통일을 위해 제거

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,62 @@
-const RouterComponent = () => {
-  return <div></div>;
+import { Route, Routes } from "react-router-dom";
+
+import {
+  LocationPage,
+  LoginPage,
+  PostDetailPage,
+  PostEditPage,
+  PostListPage,
+  PostSearchPage,
+  PostWritePage,
+} from "@/pages";
+
+import AuthRoute from "./AuthRoute";
+
+const RouterComponent: React.FC = () => {
+  return (
+    <Routes>
+      <Route
+        path="/login"
+        element={<LoginPage />}
+      />
+      <Route
+        path="/location/:id"
+        element={
+          <AuthRoute>
+            <LocationPage />
+          </AuthRoute>
+        }
+      />
+      <Route
+        path="/list"
+        element={<PostListPage />}
+      />
+      <Route
+        path="/post/:id"
+        element={<PostDetailPage />}
+      />
+      <Route
+        path="/postedit/:id"
+        element={
+          <AuthRoute>
+            <PostEditPage />
+          </AuthRoute>
+        }
+      />
+      <Route
+        path="/postcreate"
+        element={
+          <AuthRoute>
+            <PostWritePage />
+          </AuthRoute>
+        }
+      />
+      <Route
+        path="/search"
+        element={<PostSearchPage />}
+      />
+    </Routes>
+  );
 };
 
 export default RouterComponent;
