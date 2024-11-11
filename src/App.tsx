2ae8bf24e2cf8f1cfcf4f0c999b0aa9624bffa4a
hellosonic-r <hellommmh@gmail.com>
2024-11-11 21:24:21 +0900

    feat: 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
 // src/App.tsx
 import React from "react";
-import { useSelector } from "react-redux";
+import { RouterProvider } from "react-router-dom";
 
-import LoginPage from "./components/LoginPage";
-import { RootState } from "./store";
+import { router } from "./routes";
 
 const App: React.FC = () => {
-  const isAuthenticated = useSelector(
-    (state: RootState) => state.auth.isAuthenticated,
-  );
-
-  return isAuthenticated ? (
-    <div className="p-4">Welcome! You are logged in.</div>
-  ) : (
-    <LoginPage />
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,62 +1,121 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, createBrowserRouter, useRouteError } from "react-router-dom";
 
 import {
+  ChattingListPage,
+  ChattingRoomPage,
   LocationPage,
   LoginPage,
+  NotificationPage,
   PostDetailPage,
   PostEditPage,
   PostListPage,
   PostSearchPage,
   PostWritePage,
+  ProfileEditPage,
+  ProfilePage,
+  RegistPage,
 } from "@/pages";
 
 import AuthRoute from "./AuthRoute";
+import DefaultLayout from "./Layouts/DefaultLayout";
 
-const RouterComponent: React.FC = () => {
-  return (
-    <Routes>
-      <Route
-        path="/login"
-        element={<LoginPage />}
-      />
-      <Route
-        path="/location/:id"
-        element={
-          <AuthRoute>
-            <LocationPage />
-          </AuthRoute>
-        }
-      />
-      <Route
-        path="/list"
-        element={<PostListPage />}
-      />
-      <Route
-        path="/post/:id"
-        element={<PostDetailPage />}
-      />
-      <Route
-        path="/postedit/:id"
-        element={
-          <AuthRoute>
-            <PostEditPage />
-          </AuthRoute>
-        }
-      />
-      <Route
-        path="/postcreate"
-        element={
-          <AuthRoute>
-            <PostWritePage />
-          </AuthRoute>
-        }
-      />
-      <Route
-        path="/search"
-        element={<PostSearchPage />}
-      />
-    </Routes>
-  );
-};
-
-export default RouterComponent;
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <DefaultLayout />,
+    ErrorBoundary: () => {
+      throw useRouteError();
+    },
+    children: [
+      {
+        index: true,
+        element: (
+          <Navigate
+            to="/post"
+            replace
+          />
+        ),
+      },
+      {
+        path: "post",
+        children: [
+          { index: true, element: <PostListPage /> },
+          {
+            path: "search",
+            element: <PostSearchPage />,
+          },
+          {
+            path: ":postId",
+            element: <PostDetailPage />,
+          },
+          {
+            path: "write",
+            element: (
+              <AuthRoute>
+                <PostWritePage />
+              </AuthRoute>
+            ),
+          },
+          {
+            path: "edit",
+            element: (
+              <AuthRoute>
+                <PostEditPage />
+              </AuthRoute>
+            ),
+          },
+        ],
+      },
+      {
+        path: "profile",
+        children: [
+          {
+            path: ":userId",
+            element: (
+              <AuthRoute>
+                <ProfilePage />
+              </AuthRoute>
+            ),
+          },
+          {
+            path: "edit",
+            children: [
+              {
+                index: true,
+                element: <ProfileEditPage />,
+              },
+              {
+                path: "location",
+                element: <LocationPage />,
+              },
+            ],
+          },
+        ],
+      },
+      {
+        path: "/chat",
+        children: [
+          { path: "list", element: <ChattingListPage /> },
+          {
+            path: ":userId",
+            element: <ChattingRoomPage />,
+          },
+        ],
+      },
+      {
+        path: "/notifications",
+        element: <NotificationPage />,
+      },
+      {
+        path: "/login",
+        // TODO: loader: ~,
+        element: <LoginPage />,
+      },
+      {
+        path: "/regist",
+        // TODO: loader: ~,
+        element: <RegistPage />,
+      },
+    ],
+  },
+]);
